f5d416b1523abfe4dfe5974e3b0b7c9f5981700b
Shamim Reza <shamimrezaone@gmail.com>
2025-10-22 17:03:09 +0600

    fix whats app number added +88 befour number

--- src/app/(WithCommonLayout)/page.tsx
+++ src/app/(WithCommonLayout)/page.tsx
@@ -14,7 +14,7 @@ const HomePage = () => {
       <EMICalculatorButton />
       <HomeSlider />
       <Services />
-      <WhatsAppFloatButton phone="01711356235"/>
+      <WhatsAppFloatButton phone="+8801711356235"/>
       <FinancialProducts />
       <HowWeWorks />
       <FeaturedArticles />
