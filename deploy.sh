80f99faf3065f0a52685c1755b26a382704baa61
Shamim Reza <shamimrezaone@gmail.com>
2025-12-17 17:47:41 +0600

    update deploy script

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# ===== CONFIG =====
+APP_NAME="finups-frontend"
+APP_DIR="/media/algorify-labs-ltd/Server/projects/finupsbd-frontend" 
+PORT=3000
+NODE_ENV="production"
+
+echo "🚀 Starting deployment..."
+
+cd $APP_DIR || exit
+echo "🔄 Pulling latest changes from Git..."
+
+git pull origin main
+
+
+
+
+echo "📦 Installing dependencies..."
+npm install 
+
+echo "🏗️ Building Next.js app..."
+npm run build
+
+echo "♻️ Restarting app with PM2..."
+
+npm start
+
+echo "✅ Deployment finished successfully! start project finupsbd.com live"
